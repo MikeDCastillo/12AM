7a364e2a3258c37dc15c3b22aa17fd68afea9b86
Alex Whitlock <alexwhitlock@CPOs-MacBook-Pro-6.local>
2017-04-10 17:09:16 -0600

    added storyboard subclass files

---
+++ 12AM/CameraViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CameraViewController.swift
+//  12AM
+//
+//  Created by Alex Whitlock on 4/10/17.
+//  Copyright © 2017 Michael Castillo. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    @IBAction func takeNewPhotoButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func usePhotoButtonTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
