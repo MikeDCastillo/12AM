e4095339559fcf96ba3b9d6a307b4e391d8792d5
Alex Whitlock <alexwhitlock@CPOs-MacBook-Pro-6.local>
2017-04-11 11:22:15 -0600

    Set up segues, buttons, outlets, etc on storyboard files. Did basic navigation setup, but kept that commented out

---
+++ PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  12AM
+//
+//  Created by Alex Whitlock on 4/11/17.
+//  Copyright © 2017 Michael Castillo. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
