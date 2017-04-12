9e47088d5fc574dfde6b6045b52d0d71973bf910
reich452 <reich452@regis.edu>
2017-04-11 20:22:05 -0600

    Added fetch function in CloudKitManager

--- 12AM/CloudKitManager.swift
+++ 12AM/CloudKitManager.swift
@@ -11,12 +11,10 @@ import CloudKit
 
 class CloudKitManager {
     
-    func saveRecord(_ record: CKRecord, completion: ((_ record: CKRecord?, _ error: Error?) -> Void)?) {
-        
-        publicDatabase.save(record, completionHandler: { (record, error) in
-            
-            completion?(record, error)
-        })
+    let publicDataBase = CKContainer.default().publicCloudDatabase
+    
+    enum CloudKitTypes: String {
+        case userType = "User"
     }
     
     func saveRecord(_ record: CKRecord, completion: ((_ record: CKRecord?, _ error: Error?) -> Void)?) {
@@ -27,4 +25,53 @@ class CloudKitManager {
         })
     }
     
+    func saveRecord(_ record: CKRecord, completion: ((_ record: CKRecord?, _ error: Error?) -> Void)?) {
+        
+        publicDatabase.save(record, completionHandler: { (record, error) in
+            
+            completion?(record, error)
+        })
+    }
+    
+    
+    // Give us everyting
+    // To fetch information from cloudKit we run a Quiery. Can be: Give me all of the objects with this type or it can be very specific using predicates. Quiery Operations - lets us handel multiple objects.
+    func fetchRecordsWithType(_ type: String,
+                              predicate: NSPredicate = NSPredicate(value: true),
+                              recordFetchedBlock: ((_ record: CKRecord) -> Void)?,
+                              completion: ((_ records: [CKRecord]?, _ error: Error?) -> Void)?) {
+        
+        var fetchedRecords: [CKRecord] = []
+        
+        let query = CKQuery(recordType: type, predicate: predicate)
+        let queryOperation = CKQueryOperation(query: query)
+        
+        let perRecordBlock = { (fetchedRecord: CKRecord) -> Void in
+            fetchedRecords.append(fetchedRecord) // passes the record
+            recordFetchedBlock?(fetchedRecord)
+        }
+        queryOperation.recordFetchedBlock = perRecordBlock
+        
+        var queryCompletionBlock: (CKQueryCursor?, Error?) -> Void = { (_, _) in }
+        
+        queryCompletionBlock = { (queryCursor: CKQueryCursor?, error: Error?) -> Void in
+            
+            if let queryCursor = queryCursor {
+                // there are more results, go fetch them
+                
+                let continuedQueryOperation = CKQueryOperation(cursor: queryCursor)
+                continuedQueryOperation.recordFetchedBlock = perRecordBlock
+                continuedQueryOperation.queryCompletionBlock = queryCompletionBlock
+                
+                self.publicDatabase.add(continuedQueryOperation)
+                
+            } else {
+                completion?(fetchedRecords, error)
+            }
+        }
+        queryOperation.queryCompletionBlock = queryCompletionBlock
+        
+        self.publicDatabase.add(queryOperation)
+    } // Bring back all the records for the specified type (all trooper records, all starship blaster records, all trooper gun record, etc.)
 }
+
