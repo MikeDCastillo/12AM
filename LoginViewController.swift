30299bb820c48dab44e3204563189fc6d1336cda
reich452 <reich452@regis.edu>
2017-04-17 11:16:23 -0600

    Added login storyboard

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -1,6 +1,6 @@
 //
 //  LoginViewController.swift
-//  
+//
 //
 //  Created by Michael Castillo on 4/9/17.
 //
@@ -10,13 +10,15 @@ import UIKit
 import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
-    
-    @IBOutlet var loginView: UIView!
 
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+     
         let fbLoginButton = FBSDKLoginButton()
         
         view.addSubview(fbLoginButton)
@@ -28,37 +30,37 @@ class LoginViewController: UIViewController {
         view.addConstraint(fbLoginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -60))
         view.addConstraint(fbLoginButton.heightAnchor.constraint(equalToConstant: 44))    }
     
+    @IBAction func SkipButtonTapped(_ sender: Any) {
+        
+        
     }
 
-     // MARK: - FBSDK Delegate protocols
+     // MARK: - Navigation
 
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+     
+     }
+ 
+}
+
+// MARK: - FBSDK Delegate protocols
+
+
+extension LoginViewController: FBSDKLoginButtonDelegate {
     
-    extension LoginViewController: FBSDKLoginButtonDelegate {
-        
-        func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-            print("Successfully logged out of Facebook")
-        }
-        
-        func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-            if error != nil {
-                print("error")
-                return
-            }
-            print("Succesfully logged into Facebook")
-        }
-        
-        /*
-         // MARK: - Navigation
-         
-         // In a storyboard-based application, you will often want to do a little preparation before navigation
-         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destinationViewController.
-         // Pass the selected object to the new view controller.
-         }
-         */
-        
-        
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Successfully logged out of Facebook")
     }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print("error")
+            return
+        }
+        print("Succesfully logged into Facebook")
+    }
+}
 
 
 
