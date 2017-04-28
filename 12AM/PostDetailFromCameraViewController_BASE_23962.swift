5ffe289f0c04729355542c3294a4a76e2eff6187
MikeDCastillo <mikecastillouvu@gmail.com>
2017-04-28 11:20:35 -0600

    cleaning up some code

---
+++ 12AM/PostDetailFromCameraViewController_BASE_23962.swift
@@ -0,0 +1,51 @@
+//
+//  PostDetailFromCameraViewController.swift
+//  12AM
+//
+//  Created by Alex Whitlock on 4/10/17.
+//  Copyright © 2017 Michael Castillo. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailFromCameraViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.imageView.image = self.image
+        captionTextField.resignFirstResponder()
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        saveImage()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var captionTextField: UITextField!
+    
+    var image: UIImage? {
+        didSet {
+            if isViewLoaded {
+                imageView.image = image
+            }
+        }
+    }
+    
+    @IBAction func unwind(segue ofType: UIStoryboardSegue) {
+    }
+    
+    func saveImage() {
+        guard let caption = captionTextField?.text,
+            let image = imageView?.image else { return }
+        PostController.sharedController.createPost(image: image, caption: caption) { (post) in
+            guard let post = post else { return }
+        }
+    }
+}
