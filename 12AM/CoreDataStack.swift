6f8e1be70328a4c5ab7d5c327cb4016e28a6acda
reich452 <reich452@regis.edu>
2017-06-23 20:11:47 -0600

    Updated CoreDataStack

--- 12AM/CoreDataStack.swift
+++ 12AM/CoreDataStack.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import CoreData
+
+class Stack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "12AM")
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { return container.viewContext }
+}
