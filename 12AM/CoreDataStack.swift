3ed5e83ec435f37ec72cf55bf0862c75e07fd4c4
reich452 <reich452@regis.edu>
2017-06-23 20:11:11 -0600

    Added CoreDataStack and Like+Convenience

---
+++ 12AM/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  12AM
+//
+//  Created by Nick Reichard on 6/23/17.
+//  Copyright © 2017 Michael Castillo. All rights reserved.
+//
+
+import Foundation
---
+++ 12AM/Like+Convenience.swift
@@ -0,0 +1,14 @@
+//
+//  Task+Convenience.swift
+//  12AM
+//
+//  Created by Nick Reichard on 6/23/17.
+//  Copyright © 2017 Michael Castillo. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Like {
+    
+}
